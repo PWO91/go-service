ed2885f3929c8fce41f3b98b7ac23ea67989a5d8
Patryk <118165570+PWO91@users.noreply.github.com>
2024-03-08 15:38:49 +0100

    Test

--- handlers/users/users.go
+++ handlers/users/users.go
@@ -1,9 +1,15 @@
 package users
 
 import (
+	"database/sql"
+	"io"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
 )
+
+func UNUSED(x ...interface{}) {}
 
 type Item struct {
 	ID     string  `json:"id"`
@@ -14,9 +20,43 @@ type Item struct {
 
 type Items []Items
 
+func SaveInMySql(Value string) {
+
+	var db *sql.DB
+
+    // Capture connection properties.
+    cfg := mysql.Config{
+        User:   "pwo91",
+        Passwd: "Arbuz!2345678",
+        Net:    "tcp",
+        Addr:   "mysql-service-pwo91.mysql.database.azure.com:3306",
+        DBName: "storage",
+		AllowNativePasswords: true,
+    }
+    // Get a database handle.
+    var err error
+    db, err = sql.Open("mysql", cfg.FormatDSN())
+    if err != nil {
+
+    }
+
+    pingErr := db.Ping()
+    if pingErr != nil {
+       
+    }
+    
+	result, err := db.Exec("INSERT INTO proces1 (details) VALUES (?)", Value)
+	if err != nil {
+      
+    }
+
+	UNUSED(result)
+	
+}
+
 func GetItems(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"foo": "bar",
+		"Status": "Running",
 	})
 }
 
@@ -25,6 +65,10 @@ func GetItem(c *gin.Context) {
 }
 
 func PostItem(c *gin.Context) {	
+	jsonData, _ := io.ReadAll(c.Request.Body)
+	myString := string(jsonData[:])
+	SaveInMySql(myString)
+
 	c.JSON(http.StatusOK, "OK")
 }
 
